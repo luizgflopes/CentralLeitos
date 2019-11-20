b6ed1bbbdcfbd8010645e276b8f4805cbe9b48b2
Audreybh22 <30940629+Audreybh22@users.noreply.github.com>
2019-11-20 20:54:12 -0300

    Tela cadastro usuario
    correção bug pacote de codigos fonte

--- src/br/newtonpaiva/centralleitos/ui/TelaCrudUsuario.java
+++ src/br/newtonpaiva/centralleitos/ui/TelaCrudUsuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.newtopaiva.centralleitos.uil;
+package br.newtonpaiva.centralleitos.ui;
 
 import br.newtonpaiva.centralleitos.modelos.Usuario;
 import java.util.List;
