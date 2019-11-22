7e386100eececd1608c069cbe016cd0a8c5e00f9
Welder Fariles <welderfariles@gmail.com>
2019-11-22 00:54:21 -0300

    Classes Ala, Leito, TpLeito, QtdLeitos, TpSang
    Commit com as classes:
    -Ala
    -Leito
    -Quantidade de Leitos
    -Tipo Leito
    Essas classes são usadas nas telas de cadastro de Leito, cadastro tipo de leito e cadastro Ala

---
+++ src/br/newtonpaiva/centralleitos/modelos/TipoSanguineo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.newtonpaiva.centralleitos.modelo;
+
+/**
+ *
+ * @author 12103162
+ */
+public enum TipoSanguineo {
+    A_POSITIVO,
+    A_NEGATIVO,
+    B_POSITIVO,
+    B_NEGATIVO,
+    AB_POSITIVO,
+    AB_NEGATIVO,
+    O_POSITIVO,
+    O_NEGATIVO
+}
