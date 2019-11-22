91caa7eb2475b36d7c49cb3f5da3ca924b5673f0
Welder Fariles <welderfariles@gmail.com>
2019-11-22 11:41:33 -0300

    Padronização das telas
    Fazendo atualização das telas, pois as mesmas não estavam no padrão apresentado na sala

--- src/br/newtonpaiva/centralleitos/ui/TelaCrudCadAla.java
+++ src/br/newtonpaiva/centralleitos/ui/TelaCrudCadAla.java
@@ -283,9 +283,9 @@ public class TelaCrudCadAla extends javax.swing.JDialog {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+                if ("Nimbus".equals(info.getName())) { 
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); 
+                break; 
                 }
             }
         } catch (ClassNotFoundException ex) {
