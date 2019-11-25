49540d9e8f60a18309136318f7a43d7ae6b12738
Audreybh22 <30940629+Audreybh22@users.noreply.github.com>
2019-11-25 00:21:33 -0300

    Criado a classe Configuracoes

---
+++ src/ br/ newtonpaiva/ centralleitos/ modelos/Configuracoes.java
@@ -0,0 +1,35 @@
+package br.newtonpaiva.centralleitos.modelos;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/**
+ *
+ * @author 12104050
+ */
+public class Configuracoes {
+    
+    public String pegaCodigoUnidade() throws IOException {
+       FileReader ler = new FileReader("configAut.txt");
+       BufferedReader reader = new BufferedReader(ler);  
+       String consulta;
+            consulta = reader.readLine();
+            if (consulta.equals(""))
+                return null;
+            else return consulta;
+        
+    } 
+    
+            
+}
